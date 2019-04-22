32f05c6c9469e9dc946a18ce656bdb58b217bdb4
denffer <denffer18@gmail.com>
2019-04-22 23:13:59 +0800

    load self.data | refine get_sublist_index

--- preprocess.py
+++ preprocess.py
@@ -8,15 +8,15 @@ class Preprocess:
         """ data is the dict from source """
         self.data = data
     
-    def get_ground_truth(self, data):
+    def get_ground_truth(self):
         """ get ground truth """
         print("Getting ground truth ...")
 
         # Splitting sentences from raw text in corpus
         ground_truth = []
-        for b, e in zip(data.begin, data.end):
+        for b, e in zip(self.data.begin, self.data.end):
             # add 1 to include the last index
-            words = data.tokenized_corpus[b:e+1]
+            words = self.data.tokenized_corpus[b:e+1]
             sentence = " ".join(words)
 
             index = [b,e]
@@ -25,50 +25,54 @@ class Preprocess:
 
         return ground_truth
 
-    def get_nltk_tokenized_sentences(self, data):
-        """ return a list of sentence -> ['sentence1', 'sentence2'...] """
+    def get_nltk_tokenized_sentences(self):
+        """ return a list of sentence -> ['sentence1', 'sentence2', ..., 'sentenceN'] """
         #print("Getting nltk result ... ")
         
         tokenizer = Tokenizer()
-        tokenized_sentences = tokenizer.text_tokenize(data.text)
+        tokenized_sentences = tokenizer.text_tokenize(self.data.text)
         return tokenized_sentences
     
     def get_tokenized_words(self, sentences):
         """ return a list of words -> [['word1','word2', ...], ['word1','word2', ...] ...] """
         return [s.split(" ") for s in sentences]
         
-
-    def find_sentence_index(self, data, tokenized_words):
+    def find_sentence_index(self, query):
         """ return the begin_index & end_index & sentences of the query words """
 
         sentences, begin, end = [], [], []
-        for words in tqdm(tokenized_words):
+        for words in tqdm(query):
 
-            sentence_index, tarket_words = self.find_sublist_index(words, data.tokenized_corpus)
-            sentence = " ".join(tarket_words)
-
-            # filter out sentences if it is too short
-            if int(sentence_index[1]) - int(sentence_index[0]) >= 5:
-                #sentence = self.clean_text(sentence)
+            try:
+                sentence, sentence_index = self.get_sublist_index(words, self.data.indexed_corpus)
                 sentences.append([sentence_index, sentence])
                 begin.append(sentence_index[0])
                 end.append(sentence_index[1])
-            else:
-                continue
-
+            except:
+                break
+        #print(begin, end, sentences)
         return begin, end, sentences
-    
-    def find_sublist_index(self, sublist, l):
-        """ find the index of sublist in a list """
+
+    def get_sublist_index(self, sublist, indexed_list):
+        """ get the index of sublist in a list """
 
         sublist_index = []
         sublist_length = len(sublist)
-        for index in (i for i,e in enumerate(l) if e == sublist[0]):
-            if l[index:index+sublist_length] == sublist:
-                sublist_index = [index,index+sublist_length-1]
+        matched_sentence = ""
+
+        # add filter to filter out sentences if it is too short
+        for index in (i for i, e in enumerate(indexed_list) if e[1] == sublist[0] and sublist_length >= 3 ):
+            # turn indexed words into words for comparison
+            l = [word[1] for word in indexed_list[index:index+sublist_length]]
+            matched_sentence = " ".join(l)
+            if l == sublist:
+    
+                indexed_l = [word for word in indexed_list[index:index+sublist_length]]
+                begin, end = indexed_l[0][0], indexed_l[-1][0]
+                sublist_index = [begin, end]
             else:
                 continue
-                
-        # E.g., sublist : ["word1", "word2", ...] sublist_index : [22, 49]
-        return sublist_index, sublist
-    
+
+        if matched_sentence and sublist_index:
+            return matched_sentence, sublist_index
+            
