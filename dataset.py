a8270daa8526d49cc0ab8cf52d32d324968dce6a
denffer <denffer18@gmail.com>
2019-04-22 23:11:42 +0800

    add __len__ | add indexed_corpus

--- dataset.py
+++ dataset.py
@@ -5,10 +5,18 @@ class TextDataset:
     def __init__(self, src):
         self.data = self.load_train_en(src)
 
-        self.text = self.data["text"].replace("\n","")
+        self.text = self.data['text'].replace("\n","")
         self.tokenized_corpus = self.text.split(" ")
+        self.indexed_corpus = self.get_indexed_corpus()
         self.begin = self.data["begin_sentence"]
         self.end = self.data["end_sentence"]
+    
+    def __len__(self):
+        return len(self.tokenized_corpus)
+    
+    def get_indexed_corpus(self):
+        """ [[0, 'word1'], [1, 'word2'], [2, 'word3'], ..., [n-1, 'wordn'] """
+        return [[i, word] for i, word in enumerate(self.tokenized_corpus)]
     
     def load_train_en(self, src):
         """ load English training set """
