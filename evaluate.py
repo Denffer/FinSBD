d27235f8a8a5635f72ba4fec67d1577475c1cfed
denffer <denffer18@gmail.com>
2019-04-30 02:35:54 +0800

    optimize process -> cut idx_tokens

--- evaluate.py
+++ evaluate.py
@@ -7,56 +7,62 @@ class Evaluate:
     def __init__(self):
         pass
 
-    def find_sentence_index(self, data, query_list):
+    def find_query_index(self, idx_tokens, query_list):
         """ return the begin_index & end_index & sentences of the query words """
-
-        idx_tokens = data.clean_idx_tokens
 
         sentences, begin, end = [], [], []
         # boundry is set as the beginning infex of the indexed_corpus -> speed up the process
-        boundary = 0
+        #print("ori:",idx_tokens)
         for words in tqdm(query_list):
             
-            sentence, sentence_index = self.get_sublist_index(idx_tokens[boundary:], words)
+            sentence, sentence_index = self.get_sublist_index(idx_tokens, words)
+
 
             if sentence and sentence_index:
                 sentences.append([sentence_index, sentence])
                 begin.append(sentence_index[0])
                 end.append(sentence_index[1])
 
-                boundary = sentence_index[1]
+                for i, tok in enumerate(idx_tokens):
+                    if tok[0] == sentence_index[1]:
+                        idx_tokens = idx_tokens[i+1:]
+                        #print("trimmed:",idx_tokens)
+                        break
+
+
         #print(begin, end, sentences)
         return begin, end, sentences
 
-    def get_sublist_index(self, indexed_list, sublist):
+    def get_sublist_index(self, idx_tokens, sublist):
         """ get the index of sublist in a list """
 
         sublist_index = []
         sublist_length = len(sublist)
         matched_sentence = ""
+        
+        for index in (i for i, e in enumerate(idx_tokens) if e[1] == sublist[0]):
 
-        # add filter to filter out sentences if it is too short
-        for index in (i for i, e in enumerate(indexed_list) if e[1] == sublist[0] and sublist_length >= 3 ):
+            if idx_tokens[index+1][1] == sublist[1]:
+                #print("hit:", sublist)
 
-            if indexed_list[index+1][1] == sublist[1] and indexed_list[index+2][1] == sublist[2]:
                 # turn indexed words into words for comparison
-                l = [word[1] for word in indexed_list[index:index+sublist_length]]
+                l = [word[1] for word in idx_tokens[index:index+sublist_length]]
                 matched_sentence = " ".join(l)
                 if l == sublist:
-    
-                    indexed_l = [word for word in indexed_list[index:index+sublist_length]]
+        
+                    indexed_l = [word for word in idx_tokens[index:index+sublist_length]]
                     begin, end = indexed_l[0][0], indexed_l[-1][0]
                     sublist_index = [begin, end]
 
                 else:
-                    continue
+                    break
             else:
                 break
 
         if matched_sentence and sublist_index:
+            #print(matched_sentence)
             return matched_sentence, sublist_index
         else:
-            #print(sublist)
             return None, None
 
     def evaluate(self, data, begin, end):
