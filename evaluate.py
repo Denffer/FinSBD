62331edeb06417bcbe44900736bb89f378239658
denffer <denffer18@gmail.com>
2019-04-30 00:55:23 +0800

    find sentence index & move everything to main

--- evaluate.py
+++ evaluate.py
@@ -1,18 +1,63 @@
 import json, argparse
-from dataset import TextDataset
-from preprocess import Preprocess
-from tokenizer import Tokenizer
-from model.rule_based import RuleBased
-from render import Render
-#import spacy
+from tqdm import tqdm
 
 class Evaluate:
     """ python version : 3.6 """
 
-    def __init__(self, src_path, filename="output.json"):
-        self.src = src_path
-        self.dst = "output/"
-        self.filename = filename
+    def __init__(self):
+        pass
+
+    def find_sentence_index(self, data, query_list):
+        """ return the begin_index & end_index & sentences of the query words """
+
+        idx_tokens = data.clean_idx_tokens
+
+        sentences, begin, end = [], [], []
+        # boundry is set as the beginning infex of the indexed_corpus -> speed up the process
+        boundary = 0
+        for words in tqdm(query_list):
+            
+            sentence, sentence_index = self.get_sublist_index(idx_tokens[boundary:], words)
+
+            if sentence and sentence_index:
+                sentences.append([sentence_index, sentence])
+                begin.append(sentence_index[0])
+                end.append(sentence_index[1])
+
+                boundary = sentence_index[1]
+        #print(begin, end, sentences)
+        return begin, end, sentences
+
+    def get_sublist_index(self, indexed_list, sublist):
+        """ get the index of sublist in a list """
+
+        sublist_index = []
+        sublist_length = len(sublist)
+        matched_sentence = ""
+
+        # add filter to filter out sentences if it is too short
+        for index in (i for i, e in enumerate(indexed_list) if e[1] == sublist[0] and sublist_length >= 3 ):
+
+            if indexed_list[index+1][1] == sublist[1] and indexed_list[index+2][1] == sublist[2]:
+                # turn indexed words into words for comparison
+                l = [word[1] for word in indexed_list[index:index+sublist_length]]
+                matched_sentence = " ".join(l)
+                if l == sublist:
+    
+                    indexed_l = [word for word in indexed_list[index:index+sublist_length]]
+                    begin, end = indexed_l[0][0], indexed_l[-1][0]
+                    sublist_index = [begin, end]
+
+                else:
+                    continue
+            else:
+                break
+
+        if matched_sentence and sublist_index:
+            return matched_sentence, sublist_index
+        else:
+            #print(sublist)
+            return None, None
 
     def evaluate(self, data, begin, end):
         """ check the percentage hit rate as compared to ground truth """
@@ -32,62 +77,3 @@ class Evaluate:
         evaluation = [begin_hit_rate, end_hit_rate]
 
         return evaluation
-
-    def run(self):
-        # get data
-        data = TextDataset(self.src)
-        # preprocess
-        p = Preprocess(data)
-        ground_truth = p.get_ground_truth()
-        results = []
-
-        # nltk as baseline
-        print("Processsing NLTK as baseline ... ")
-        nltk_sentences = p.get_nltk_tokenized_sentences()
-        query_list = p.get_tokenized_words(nltk_sentences)
-        
-        begin, end, indexed_sentences = p.find_sentence_index(query_list)
-        evaluation = self.evaluate(data, begin, end)
-        result = {"key":"nltk", "begin":begin, "end":end, "sentences":indexed_sentences, "evaluation":evaluation}
-        results.append(result)
-
-        #rule_based methods
-        rule_based = RuleBased()
-
-        print("Processsing rule based (Subject) ... ")
-        filtered_sentences = rule_based.filter_subject(nltk_sentences)
-        words = p.get_tokenized_words(filtered_sentences)
-        begin, end, indexed_sentences = p.find_sentence_index(words)
-        evaluation = self.evaluate(data, begin, end)
-        result = {"key":"has_subject", "begin":begin, "end":end, "sentences":indexed_sentences, "evaluation":evaluation}
-        results.append(result)
-
-        print("Processsing rule based (Verb)... ")
-        words = p.get_tokenized_words(filtered_sentences)
-        begin, end, indexed_sentences = p.find_sentence_index(words)
-        evaluation = self.evaluate(data, begin, end)
-        result = {"key":"has_verb", "begin":begin, "end":end, "sentences":indexed_sentences, "evaluation":evaluation}
-        results.append(result)
-
-        print("Processsing rule based (Subject & Verb)... ")
-        filtered_sentences = rule_based.filter_verb(filtered_sentences)
-        words = p.get_tokenized_words(filtered_sentences)
-        begin, end, indexed_sentences = p.find_sentence_index(words)
-        evaluation = self.evaluate(data, begin, end)
-        result = {"key":"has_subjectVerb", "begin":begin, "end":end, "sentences":indexed_sentences, "evaluation":evaluation}
-        results.append(result)
-
-        # write result
-        print("Writing data to: " + str(self.dst) + "\033[1m" + str(self.filename) + "\033[0m")
-        render = Render(self.dst, self.filename, data, ground_truth, results)
-        render.save()
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--src", type=str, required=True, help="path for the source")
-    parser.add_argument("--filename", type=str, required=True, help="name for the file")
-    args = parser.parse_args()
-
-    main = Evaluate(src_path=args.src,filename=args.filename)
-    main.run()
