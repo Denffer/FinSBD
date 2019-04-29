feeae84ee2e97e6741f881bd5a601183d736f0e1
denffer <denffer18@gmail.com>
2019-04-30 00:50:11 +0800

    merge preprocess.py

--- dataset.py
+++ dataset.py
@@ -1,22 +1,89 @@
 import re, json
 
 class TextDataset:
-    """ load data from paths.json """
+    """ load data from paths.json | preprocess """
     def __init__(self, src):
-        self.data = self.load_train_en(src)
+        self.raw_data = self.load_train_en(src)
 
-        self.text = self.data['text'].replace("\n","")
-        self.tokenized_corpus = self.text.split(" ")
-        self.indexed_corpus = self.get_indexed_corpus()
-        self.begin = self.data["begin_sentence"]
-        self.end = self.data["end_sentence"]
+        self.text = self.raw_data['text'].replace("\n","")
+        #print(self.text[157:1239])
+        self.clean_text = self.get_clean_text(self.text).strip()
+
+        self.tokens = self.text.split(" ")
+        self.idx_tokens = self.get_indexed_tokens()
+        self.clean_idx_tokens = self.get_clean_tokens(self.idx_tokens)
+        
+        l = self.clean_text.split(" ")
+
+        self.begin = self.raw_data['begin_sentence']
+        self.end = self.raw_data['end_sentence']
+
+        self.ground_truth = self.get_ground_truth(self.begin, self.end, self.clean_idx_tokens)
     
     def __len__(self):
-        return len(self.tokenized_corpus)
+        return len(self.tokens)
     
-    def get_indexed_corpus(self):
+    def get_indexed_tokens(self):
         """ [[0, 'word1'], [1, 'word2'], [2, 'word3'], ..., [n-1, 'wordn'] """
-        return [[i, word] for i, word in enumerate(self.tokenized_corpus)]
+        return [[i, word] for i, word in enumerate(self.tokens)]
+
+    def get_clean_text(self, text):
+        """ remove noise """
+        text = re.sub('[0-9]+', '', text)
+        text = re.sub('\s+', ' ', text)
+        
+        #remove unnecessary full stop
+        text = re.sub('(\.\s)+', '. ', text)
+        return text
+
+    def get_clean_tokens(self, tokens):
+        """ remove noise """
+        clean = []
+        #regex = re.compile('[0-9]+')
+        for idx_tok in tokens:
+            idx_tok[1] = re.sub('[0-9]+', '', idx_tok[1])
+            if idx_tok[1] == "":
+                pass
+            else:
+                clean.append(idx_tok)
+
+        i = 0
+        while i < len(clean)-1:
+            if clean[i][1] == clean[i+1][1] and clean[i+1][1] == ".":
+                del clean[i+1]
+            else:
+                i = i+1        
+        #print(clean)
+        return clean
+
+    def get_ground_truth(self, begin, end, tokens):
+        """ get ground truth """
+        print("Getting ground truth ...")
+
+        ground_truth = []
+        # Splitting sentences from raw text in corpus
+        for b, e in zip(begin, end):
+
+            boundary = 0
+            flag = 0
+            sentence = []
+            for token in tokens:
+                boundary += 1
+                if token[0] == e:
+                    sentence.append(token)
+                    flag = 0
+                    break
+                elif token[0] == b or flag == 1:
+                    sentence.append(token)
+                    flag = 1
+                else:
+                    flag = 0
+                    pass
+
+            tokens = tokens[boundary:]
+            ground_truth.append(sentence)
+
+        return ground_truth
     
     def load_train_en(self, src):
         """ load English training set """
